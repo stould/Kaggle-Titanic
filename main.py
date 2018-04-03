4e20a671d787674d924ba3fe045759d4325079ef
Vinícius Zibetti Resko <vinirresko@gmail.com>
2018-04-03 17:35:36 -0300

    New changes added real probability.
    It is needed to change the values in order to get better results.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pandas
 import random
 
+def getByProb(prob):
+  num = prob
+  den = 100.0
+  rand_num = random.random()
+  if(rand_num < (num/den)):
+    return True
+  return False
 
-def judgeByAge(age, l, r):
+def judgeByAge(age, pa, pb):
 	ans = -1
 	if (int(age) < 8 or int(age) > 44):
-		ans = random.randint(l, r)
+		ans = getByProb(pa)
 	else:
-		ans = random.randint(l, r - int((r - l) / 3))
+		ans = getByProb(pb)
 	if (ans == 1):
 		return True
 	else:
@@ -18,40 +25,51 @@ test = pandas.read_csv('test.csv')
 
 workers = ['Rev', 'Capt']
 
-test['Fare'].fillna(test['Fare'].mean(), inplace=True)
+#test['Fare'].fillna(test['Fare'].mean(), inplace=True)
 test['Age'].fillna(round(test['Age'].mean()), inplace=True)
 
 ids = []
 surv = []
 
 for index, row in test.iterrows():
-	survived = False
-	for i in range(len(workers)):
-		if (workers[i] in row["Name"]):
-			survived = True
-			break
-	if (survived == False):  #classes {1,2,3}
-		if (int(row["Pclass"]) == 3):
-			if (row["Sex"] == "male"):
-				survived = judgeByAge(row["Age"], 1, 6)
-			else:
-				survived = judgeByAge(row["Age"], 1, 5)
-		elif (int(row["Pclass"]) == 2):
-			if (row["Sex"] == "male"):
-				survived = judgeByAge(row["Age"], 1, 4)
-			else:
-				survived = judgeByAge(row["Age"], 1, 3)
-		else:
-			if (row["Sex"] == "male"):
-				survived = judgeByAge(row["Age"], 1, 3)
-			else:
-				survived = judgeByAge(row["Age"], 1, 2)
-	if (survived == True):
-		ids += [row["PassengerId"]]
-		surv += ["1"]
-	else:
-		ids += [row["PassengerId"]]
-		surv += ["0"]
+  canGo = True
+  survived = False
+  if(canGo and row["Fare"] == ""):
+    if (row["Sex"] == "male"):
+      survived = judgeByAge(row["Age"], 20, 15)
+    else:
+      survived = judgeByAge(row["Age"], 85, 75)
+    canGo = False
+	
+  for i in range(len(workers)):
+    if(canGo != True):
+      break
+    if (workers[i] in row["Name"]):
+      survived = True
+      break
+  		
+  if (canGo == True):  #classes {1,2,3}
+    if (int(row["Pclass"]) == 3):#Third class, almost all died.
+      if (row["Sex"] == "male"):
+        survived = judgeByAge(row["Age"], 20, 15)
+      else:
+        survived = judgeByAge(row["Age"], 30, 25)
+    elif (int(row["Pclass"]) == 2):#Second class, more less half survived
+      if (row["Sex"] == "male"):
+        survived = judgeByAge(row["Age"], 42, 36)
+      else:
+        survived = judgeByAge(row["Age"], 52, 45)
+    else:#First class, almost everyone survived
+      if (row["Sex"] == "male"):
+        survived = judgeByAge(row["Age"], 52, 45)
+      else:
+        survived = judgeByAge(row["Age"], 65, 60)
+      if (survived == True):
+        ids += [row["PassengerId"]]
+        surv += ["1"]
+      else:
+        ids += [row["PassengerId"]]
+        surv += ["0"]
 
 output = pandas.DataFrame(
     {
